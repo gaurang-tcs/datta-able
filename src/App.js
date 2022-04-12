c72d88f1005fe45078235511491e05be2a6926b9
gaurang-tcs <98448274+gaurang-tcs@users.noreply.github.com>
2022-04-12 09:33:22 +0530

    fix and updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
-//import SideMenu from './component/sidemenu/drawer.component';
+
 
 import TemporaryDrawer from './component/drawer/drawer.component';
 
